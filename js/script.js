279217b161e3f1542ba3a002794d415a578d4ddb
Charlotte S <93612273+cbsumo305@users.noreply.github.com>
2022-03-02 11:38:39 -0600

    Wrote function to only allow unique guesses

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const btnPlayAgain = document.querySelector(".play-again");
 
 let word = "magnolia";
+let alreadyGuessed = [];
 
 //Create and name a function to update the paragraph’s innerText for the “words-in-progress” element with circle symbols (●) to represent each letter in the word. The symbols will stay on the screen until the correct letter is guessed (in a future step). Hint: Copy and paste the ● symbol into your code!
 
@@ -29,5 +30,29 @@ btnGuess.addEventListener("click", function (e) {
     e.preventDefault();
     const ltr = txtLetter.value;
     txtLetter.value = "";
+    validateInput(ltr);
 })
+
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input === "") {
+        message.innerText = "Please enter a letter from A to Z.";
+    } else if (input.length > 1) {
+        message.innerText = "You entered more than 1 letter";
+    } else if (input.match(acceptedLetter) === null) {
+        message.innerText = "Please enter a letter from A to Z.";
+    } else {
+        message.innerText = "";
+        makeGuess(input);
+    }
+}
+
+const makeGuess = function (input) {
+    const ltr = input.toUpperCase();
+    if (alreadyGuessed.includes(ltr)) {
+        message.innerText = "You already guessed that letter, silly. Try again.";
+    } else {
+        alreadyGuessed.push(ltr);
+    }
+}
 
